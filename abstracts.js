d3949ce8325a656e8b94ad8ef2eb3b52f2f00185
Максим <maksimbobrov@MacBook-Air-Maksim.local>
2021-04-06 00:54:54 +0700

    end 18 lesson

--- abstracts.js
+++ abstracts.js
@@ -8,12 +8,12 @@ const personalMovieDB = {
     privat: false
 };
 
-  
+
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотреннх фильмов ?', ''),
-          b = prompt('На сколько оцените его', '');
+        b = prompt('На сколько оцените его', '');
 
-    if( a != null && b != null && a != '' && b != ''  && a.length < 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('done!');
     } else {
@@ -28,9 +28,9 @@ if (personalMovieDB.count < 10) {
     console.log('просмотрено довольно мало фильмов ');
 } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
     console.log('вы классический зритель !');
-}else if (personalMovieDB.count > 30 ) {
+} else if (personalMovieDB.count > 30) {
     console.log('вы киноман ');
-} else{
+} else {
     console.log('произошла ошибка !');
 }
 
@@ -49,7 +49,7 @@ console.log(personalMovieDB);
 
 
 function showFirstMessage(text) {
-    console.log (text);
+    console.log(text);
     console.log(num);
 }
 
@@ -57,8 +57,8 @@ function showFirstMessage(text) {
 showFirstMessage("hello world !");
 console.log(num);
 
-function calc(a, b ) {
-    return(a+b);
+function calc(a, b) {
+    return (a + b);
 }
 
 console.log(calc(4, 3));
@@ -79,7 +79,9 @@ const logger = function () {
 
 logger();
 
-const calc = (a , b) => {return a + b};
+const calc = (a, b) => {
+    return a + b
+};
 
 
 
@@ -100,53 +102,152 @@ const calc = (a , b) => {return a + b};
 
 
 
-function calc(a , b) {
+function calc(a, b) {
     return (a * b);
-   
-     
-   }
-   
-   console.log(calc(7, 6 ));
-   console.log(calc(10 , 10));
-   
-   
-   const max = {
-   
-       firstName:maxim,
-       old:19,
-       logger:lon,
-   }
-   
-   console.log(max.firstName);
-   
-   
-   const str = 'testrdytkfyu';
-   
-   const arr = [
-       1, 2, 4
-   ];
-   
-   console.log(str.toLocaleUpperCase());
-   
-   let fruit = "some fruit";
-   
-   console.log(fruit.indexOf("f"));
-   
-   const logg = "Hello world"
-   
-   console.log(logg.slice(0, 5));
-   
-   console.log(logg.substring(6, 11));
-   
-   console.log(logg.substr(6, 5));
-   
-   const num = 12.3;
-   
-   console.log(Math.round);
-   
-   
-   const test = "12.2px";
-   
-   console.log(parseInt(test));
-   
-   console.log(parseFloat(test));
+
+
+}
+
+console.log(calc(7, 6));
+console.log(calc(10, 10));
+
+
+const max = {
+
+    firstName: maxim,
+    old: 19,
+    logger: lon,
+}
+
+console.log(max.firstName);
+
+
+const str = 'testrdytkfyu';
+
+const arr = [
+    1, 2, 4
+];
+
+console.log(str.toLocaleUpperCase());
+
+let fruit = "some fruit";
+
+console.log(fruit.indexOf("f"));
+
+const logg = "Hello world"
+
+console.log(logg.slice(0, 5));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.3;
+
+console.log(Math.round);
+
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+
+console.log(parseFloat(test));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////
+
+
+
+
+
+
+let numberOfFilms
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '')
+
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+    }
+}
+
+start();
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+
+function rememberMyFilms() {
+
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотреннх фильмов ?', ''),
+            b = prompt('На сколько оцените его', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done!');
+        } else {
+            console.log('error!');
+            i--;
+        }
+
+    }
+}
+
+rememberMyFilms();
+
+
+
+function detectPersonalMovie() {
+
+    if (personalMovieDB.count < 10) {
+        console.log('просмотрено довольно мало фильмов ');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log('вы классический зритель !');
+    } else if (personalMovieDB.count > 30) {
+        console.log('вы киноман ');
+    } else {
+        console.log('произошла ошибка !');
+    }
+}
+
+detectPersonalMovie();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под ноиером ${i}`);
+
+    }
+}
+
+writeYourGenres();
