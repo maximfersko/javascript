1c43a5050d5a9894777978466b8b8ccb560675f7
Максим <maksimbobrov@MacBook-Air-Maksim.local>
2021-04-06 13:11:28 +0700

    end lesson 20

--- abstracts.js
+++ abstracts.js
@@ -251,3 +251,141 @@ function writeYourGenres() {
 }
 
 writeYourGenres();
+
+
+
+
+
+//////////////////
+
+
+
+// сам делал тест 
+
+
+
+const questionFilms = prompt('сколько вам лет ?')
+
+
+const obj = {
+
+    mon: [],
+    answer: questionFilms
+}
+
+console.log(obj);
+
+if (questionFilms > 6) {
+    console.log("error!");
+} else if (questionFilms >= 18) {
+    console.log("done!")
+}
+
+
+
+
+
+///////////////////
+
+
+
+
+
+
+
+
+
+
+function first() {
+    //do something
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок !');
+}
+
+learnJS('JavaScript', done);
+
+
+
+
+//////////////////////
+
+
+
+
+
+
+const options = {
+    naMe: 'test',
+    width: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function() {
+        console.log('test');
+    }
+}
+
+options.makeTest();
+
+
+
+const {border, bg} = options.colors;
+
+
+
+
+console.log(Object.keys(options).length);
+
+console.log(options["colors"]["border"])
+
+const inf = {
+    myGF:{
+        Name:'ksusha',
+        secondName:'Minaeva',
+        old:14
+    },
+    colors:white,
+    gender:o 
+}
+
+console.log(Object.keys(inf).length);
+
+
+//console.log(options.colors.bg);
+
+//delete options.naMe
+
+//console.log(options);
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for  (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
+            counter++;
+        }
+    }else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+    
+}
+
+console.log(counter);
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 'use strict'
 
+const arr = [1, 2, 3, 4, 5, 6, 8];
